b49fd44cd1de56be6aff815fbebecff852ffbe86
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-04-03 23:07:21 +0200

    generic variables

--- instance/main.tf
+++ instance/main.tf
@@ -1,9 +1,9 @@
 module "upServer" {
   source             = "./upServer"
-  name_sshkey        = "hugoSSHkey"
-  file_sshkey        = "~/.ssh/id_rsa.pub"
-  application_key    = "d4276d7de1f4a2ef"
-  application_secret = "749f012277db5fb5baad53cc2ccee43e"
-  consumer_key       = "c61a14e4032262269a32f7259deb5772"
-  instances      = var.instances
+  name_sshkey        = "Name you want to key to take in your ovh account"
+  file_sshkey        = "Path to a local public key"
+  application_key    = "Generate app key"
+  application_secret = "Generate app secret"
+  consumer_key       = "Generate consumer key"
+  instance_name      = "Name of the instance that the module will create"
 }
